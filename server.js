d1b2e21ade12030dd6cbcc0e7b99654a8532fbe5
AlejandroJesusSerrano <alejandrojserrano@hotmail.com>
2022-07-14 21:26:46 -0300

    desafio mareador jejeje...

--- server.js
+++ server.js
@@ -5,29 +5,55 @@ const fs = require ('fs')
 const file = './files/products.json'
 
 class Container {
-    constructor (productsList, fileJson = file) {
-        this.productsList = productsList;
-        this.fileJson = fileJson;
-    }
-
-    getAll(productsArray){
-        async function read(){
-          try{
-            const fileContent = await fs.promises.readFile('./files/products.json', 'utf-8');
-            
-            const productsList = JSON.parse(fileContent);
-            let {products} = productsList;
-            
-            console.log (products);
-          
-        } catch (err) {
-            console.log ('lo setimos a habido un error', err);
-          };
-        };
+    constructor (fileJson) {
         
-        read();
+        this.fileJson = fileJson;
+    }      
+    
+
+    getAll = async() => {
+          try{
+            
+                return await fs.promises.readFile(file, 'utf-8');
+
+            } catch (err) {
+                
+                console.log ('lo setimos a habido un error', err);
+            
+            };
+        };
+        // const productsList = JSON.parse(fileContent);
+        // let {products} = productsList;
+        // console.log(products)
+        // return products
+
+    getRandom = async() =>{
+         try{
+            await fs.promises.readFile(file, 'utf-8')
+            //  let product = fs.readFile(file, 'utf-8');
+            .then (product => {
+                let randomObject = JSON.parse(product)//[Math.floor(Math.random() * product.length)];
+                let random = Math.floor(Math.random()*randomObject.length);
+                console.log(randomObject[random]) //esto si muestra por consola
+                return randomObject[random] // no lo puedo ver en el navegador
+            })
+        } catch (error) { 
+            console.log (`Lo sentimas a habido un error`, error)
+        }
     };
-};
+// else {
+//             const fileContent = fs.readFileSync('./files/products.json', 'utf-8');
+//             const productsList = JSON.parse(fileContent);
+
+//             let {products} = productsList;
+
+//             
+
+//             res.send(randomObject)
+//         };
+//     });
+// });
+}
 
 app.get('/', (req, res) =>{
     res.send({msj:"aprendiendo Express"})
@@ -35,27 +61,17 @@ app.get('/', (req, res) =>{
 
 app.get('/products', (req, res) =>{
     const data = new Container ("./files/products.json")
-    res.send(data.getAll())
+    data.getAll().then((product) => res.send(JSON.parse(product)))
 });
 
-app.get('/productRandom', (req, res) => {
+app.get('/productRandom', (req, res) =>{
+    const data = new Container ("./files/products.json")
+    res.send(data.getRandom())//.then((products) => res.send(JSON.parse(product)))
+});
+// app.get('/productRandom', (req, res) => {
     
-    fs.readFile(productsFile, (error) => {
-        if (error){
-            console.log (`Lo sentimos ha habido un error!!!.
-            El archivo a sido renombrado, o no Existe`)
-        } else {
-            const fileContent = fs.readFileSync(productsFile, 'utf-8');
-            const productsList = JSON.parse(fileContent);
+//     fs.readFile('./files/products.json', (error) => {
 
-            let {products} = productsList;
-
-            let randomObject = products[Math.floor(Math.random() * products.length)];
-
-            res.send(randomObject)
-        };
-    });
-});
 
 const PORT = 8080;
 const server = app.listen (PORT, () => {
